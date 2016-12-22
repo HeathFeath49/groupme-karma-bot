70eb4c32d061a6a1bc6f86544d63d953326e30fc
zach111694 <zach111694@gmail.com>
2016-12-21 19:26:02 -0500

    Testing /help function

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,19 @@ var request = require('request');
 
 /* GET home page. */
 router.post('/process_message', function(req, res, next) {
-  return request({
-  	method: 'POST',
-  	url: 'https://api.groupme.com/v3/bots/post',
-  	json: {
-  		"bot_id": "821341b4b98c095d00042f97cc",
-  		"text"	: "Hello world"
-  	}
-  });
+	console.log(req.body);
+	if(req.body.sender_type == "user"){
+		if(req.body.text == "/help"){
+			return request({
+				method: 'POST',
+				url: 'https://api.groupme.com/v3/bots/post',
+				json: {
+					"bot_id": "821341b4b98c095d00042f97cc",
+					"text"	: "What can I help you with?"
+				}
+			});
+		}
+	}
 });
-
-
-
 
 module.exports = router;
