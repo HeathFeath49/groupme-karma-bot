a207414de3eaf1accecf9572b9ee0e1575190bc0
HeathFeath49 <hstarkie@lions.molloy.edu>
2016-12-31 23:05:58 -0500

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var resources = require('../resources/groupme-karma-bot.json');
+var resources = require('../resources/bot-info.json');
 
 /* GET home page. */
 router.post('/process_message', function(req, res, next) {
 	console.log(req.body);
 	if(req.body.sender_type == "user"){
-		if(req.body.text == "/help"){
+		if(req.body.text == "yo"){
 			return request({
 				method: 'POST',
 				url: 'https://api.groupme.com/v3/bots/post',
 				json: {
 					"bot_id": resources.bot_id,
-					"text"	: "What can I help you with?"
+					"text"	: "What's cooking?"
 				}
 			});
 		}
