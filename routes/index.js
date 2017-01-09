b8dfb6658e990beb9b77fec67afaea2b1d2369db
HeathFeath49 <hstarkie@lions.molloy.edu>
2017-01-08 20:48:23 -0500

    updated curse data and added reprimand function

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var resources = require('../resources/bot-info.json');
+var curseData = require('../resources/curse-data.json');
 
 //obj of member objects
 var members = {};
@@ -41,7 +42,10 @@ function isCurse(word,curObj){
 // adds/increments members curseHistory object
 //TESTED: PASSED(Mock data) 
 function updateCurseObj(name,curse){
-  var cur = curse.toLowerCase();
+  	if(!(name in members)){
+  		addMember(name);
+  	}
+  	var cur = curse.toLowerCase();
 	var ch = members[name].curseHistory;
 	if(ch[cur] === undefined){
 	  ch[cur] = 1;
@@ -51,14 +55,28 @@ function updateCurseObj(name,curse){
 	}
 }
 
+//bot sends message to chat reprimanding user
+//and letting them know what bad word they used
+function reprimand(user,badWord){
+	return request({
+		method: 'POST',
+		url: 'https://api.groupme.com/v3/bots/post',
+		json:{
+				"bot_id": resources.bot_id,
+				"text"	: "Hey "+user+"! Watch your language! (bad word: "+badWord+")"
+			}
+	});
+}
+
 //breaks message up by individual words and checks
 //each word to see if it is a curse
 //TESTED: Mock data
 function processMessage(user,message){
 	var wordArr = message.split(" ");
 	for(var i=0;i<wordArr.length;i++){
-	   if(isCurse(wordArr[i],curseWords)){
+	   if(isCurse(wordArr[i],curseData)){
 	    updateCurseObj(user,wordArr[i]);
+	    reprimandUser(user,wordArr[i]); 
 	   }
 	}
 }
@@ -66,7 +84,13 @@ function processMessage(user,message){
 
 /* GET home page. */
 console.log("up and running!");
-router.post('/process_message', function(req, res, next) {
+router.post('/process_message',function(req,res,next){
+	if(req.body.sender_type == "user"){
+		process_message(req.body.name,req.body.text);	
+	}
+})
+
+/*router.post('/process_message', function(req, res, next) {
 	console.log(req.body);
 	if(req.body.sender_type == "user"){
 		if(req.body.text == "yo"){
@@ -80,6 +104,6 @@ router.post('/process_message', function(req, res, next) {
 			});
 		}
 	}
-});
+});*/
 
 module.exports = router;
